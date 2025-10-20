b107c5666070aba3694fc24fef5b6d9925c410ad
IyaMariana <irumiched@ucvvirtual.edu.pe>
2025-10-20 09:57:21 -0500

    kkkkkkkkkkk

--- Config/Config.php
+++ Config/Config.php
@@ -3,6 +3,7 @@ const base_url = "http://localhost/bibliSM/";
 const host = "localhost";
 const user = "root";
 const pass = "2025";
-const db = "biblioteca";
+const db = "bibliotecaqwertyui";
 const charset = "charset=utf8";
+//prueba// jkkkkkkkkkkkkkkkkkkkkkk
 ?>
